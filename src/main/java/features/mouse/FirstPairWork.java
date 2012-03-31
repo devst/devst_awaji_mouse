b186a7f9cd8518d9abf04c906604d55a2db53a22
unknown <reimei@Jae-Jin.(none)>
2012-03-31 14:07:31 +0900

    ikigomi commit

---
+++ src/main/java/features/mouse/FirstPairWork.java
@@ -0,0 +1,15 @@
+package features.mouse;
+
+public class FirstPairWork implements features.FirstPairWork {
+
+	@Override
+	public String getMembers() {
+		return "nara_reimei & Aki";
+	}
+
+	@Override
+	public String getIkigomi() {
+		return "楽しく焦らずに";
+	}
+
+}
