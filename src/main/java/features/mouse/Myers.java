991ae950291a1984789d3c96c1dd84b6ff9bdbb5
Aki <aki_@Aki-no-MacBook-Air.local>
2012-04-01 23:27:24 +0900

    fix Q.

--- src/main/java/features/mouse/Myers.java
+++ src/main/java/features/mouse/Myers.java
@@ -3,9 +3,19 @@ package features.mouse;
 public class Myers implements features.Myers {
 
 	@Override
-	public String getName(int arg0, int arg1, int arg2) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	public String getName(int lineA, int lineB, int lineC) {
+
+		if (lineA <= 0 || lineB <= 0 || lineC <= 0) {
+			throw new IllegalArgumentException();	
+		}
+		
+		if (lineA == lineB && lineB == lineC) {
+			return "正三角形";
+		} else if (lineA == lineB || lineB == lineC || lineC == lineA) {
+			return "二等辺三角形";
+		} else {
+			return "不等辺三角形";
+		}
 	}
 
 }
--- src/test/java/features/mouse/MyersTest.java
+++ src/test/java/features/mouse/MyersTest.java
@@ -2,9 +2,59 @@ package features.mouse;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import features.mouse.Myers;
 
 public class MyersTest {
 
-	// 
+	Myers target;
+	
+	@Before
+	public void setUp() throws Exception {
+		target = new Myers();
+	}
+
+	/**
+	 * 正三角形:すべての辺が同じ長さ
+	 */
+	@Test
+	public void 正三角形(){
+		assertEquals("正三角形", target.getName(1, 1, 1));
+	}
+	
+	/**
+	 * 二等辺三角形:2つの辺だけが同じ
+	 */
+	@Test
+	public void 二等辺三角形(){
+		assertEquals("二等辺三角形", target.getName(2, 2, 1));
+	}
+	
+	/**
+	 * 不等辺三角形:すべての辺が違う長さ
+	 */
+	@Test
+	public void 不等辺三角形(){
+		assertEquals("不等辺三角形", target.getName(1, 2, 3));
+	}
+
+	/**
+	 * 不等辺三角形:すべての辺が違う長さ
+	 * 三角形にならない場合: いずれかの辺が0
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void 非三角形_value0(){
+		target.getName(0, 1, 1);
+	}
+
+	/**
+	 * 不等辺三角形:すべての辺が違う長さ
+	 * 三角形にならない場合: いずれかの辺がマイナス値
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void 非三角形_value_minus(){
+		target.getName(-1, 1, 1);
+	}
+
 }
