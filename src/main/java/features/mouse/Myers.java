16e706a1389e2c9fd0646fbbfc8d142af00ac365
Aki <aki_@Aki-no-MacBook-Air.local>
2012-04-01 23:50:10 +0900

    fix encoding

--- src/main/java/features/mouse/Myers.java
+++ src/main/java/features/mouse/Myers.java
@@ -11,11 +11,11 @@ public class Myers implements features.Myers {
 		}
 		
 		if (lineA == lineB && lineB == lineC) {
-			return "正三角形";
+			return "";
 		} else if (lineA == lineB || lineB == lineC || lineC == lineA) {
-			return "二等辺三角形";
+			return "";
 		} else {
-			return "不等辺三角形";
+			return "";
 		}
 	}
 
